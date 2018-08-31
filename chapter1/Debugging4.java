f8f2ea96dcc4837610bfe481b6defbd6fdacbd9e
Amia Combs <ac116232@students.davenport.k12.ia.us>
2018-08-31 09:53:18 -0500

    Debug

---
+++ chapter1/Debugging4.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+import javax.swing.JOptionPane;
+
+public class Debugging4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    JOptionPane.showMessageDialog(null,"First GUI program");
+	}
+
+}
